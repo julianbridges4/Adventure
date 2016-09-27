cdd580125342d7e3aa9ac2f0e001f1d6bb9d2141
JULIAN BRIDGES <julianbridges01@gmail.com>
2016-09-27 08:31:12 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :activities
+  resources :states
+  root 'welcome#index'
+
+  get 'about' => 'welcome#about'
+
+  get 'ilovetocode' => 'welcome#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
